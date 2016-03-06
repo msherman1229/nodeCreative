36ac1d91852b4c0869a523fef87e1d2e773a4b0c
msherman1229 <msherman1229@gmail.com>
2016-03-06 00:11:44 +0000

    working algorithm

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,13 +18,91 @@ function resultFetcher ($http) {
 
 function mainCtrl ($scope, resultFetcher) {
 	$scope.questionSubmit = function (user) {
-		console.log(user.choice);
-		console.log(user.choice2);
-		console.log(user.choice3);   
+		var q1 = $scope.user.choice; 
+		var q2 = $scope.user.choice2; 
+		var q3 = $scope.user.choice3; 
+		var q4 = $scope.user.choice4; 
+		calculator(q1, q2, q3, q4);    
 	};
 	resultFetcher.get()
 		.then(function (data) { 
 			console.log(data);
 			$scope.results = data;  
 		}); 
+}
+
+function calculator ( q1, q2, q3, q4)
+{
+	var candidates = [
+		{name:"Hillary Clinton", count: 0}, 
+		{name:"Ted Cruz", count: 0}, 
+		{name:"John Kasich", count: 0}, 
+		{name:"Marco Rubio", count: 0}, 
+		{name:"Bernie Sanders", count: 0}, 
+		{name:"Donald Trump", count: 0}
+	]
+	switch (q1) {
+	case "1":
+		candidates[4]["count"]++;
+		break;   
+	case "2":
+		candidates[0]["count"]++;
+		candidates[2]["count"]++;
+		break;  
+	case "3":
+		candidates[3]["count"]++;
+		candidates[1]["count"]++;
+		break;
+	case "4":
+		candidates[5]["count"]++; 
+		break;
+	default:
+	}
+	switch (q2) {
+	case "1":
+		candidates[0]["count"]++;
+		break; 
+	case "2":
+		candidates[4]["count"]++;
+		break;
+	case "3":
+		candidates[3]["count"]++;
+		candidates[1]["count"]++;
+		candidates[5]["count"]++;
+		candidates[2]["count"]++;
+		break;
+	default:
+	}
+	switch (q3) {
+	case "1":
+		candidates[0]["count"]++;
+		candidates[2]["count"]++;
+		break; 
+	case "2":
+		candidates[4]["count"]++;
+		candidates[1]["count"]++;
+		candidates[3]["count"]++;
+		break;
+	case "3":
+		candidates[5]["count"]++;
+		break;
+	case "4":
+		candidates[5]["count"]++;
+		break;
+	default:
+	}
+	switch (q4) {
+	case "1":
+		candidates[0]["count"]++;
+		candidates[4]["count"]++;
+	case "2":
+		candidates[5]["count"]++;
+	case "3":
+		candidates[1]["count"]++;
+		candidates[3]["count"]++;
+		candidates[2]["count"]++;
+	default:
+	}
+	candidates.sort(function(a,b) {return b.count-a.count});
+	console.log(candidates[0]["name"]); 
 }
